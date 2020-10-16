73342d392761dd7d0c2e5e6287e7943def77a98f
razibravo76 <60962547+razibravo76@users.noreply.github.com>
2020-10-16 11:26:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var set_editor = prompt("select editor html or bootstrap");
 function update() {
   var idoc = document.getElementById("iframe").contentWindow.document; // setting iframe as an output window
 
@@ -5,25 +6,45 @@ function update() {
   idoc.write(editor.getValue()); // getting the value from editor and displaying it on iframe
   idoc.close();
 }
-
+var nom_html = `<!DOCTYPE html>
+<html>
+  <head>
+    <title> Document </title>
+  </head>
+  <body>
+    <p> Welcome </p>
+  </body>
+</html>`;
+var boot_html = `<!DOCTYPE html>
+<html>
+  <head>
+    <title> Document </title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <p> Welcome </p>
+  </body>
+</html>`
 //setting up the editor
 function setupEditor() {
   window.editor = ace.edit("editor"); //making editor div as an editor
   editor.setTheme("ace/theme/tomorrow_night_blue"); // theme of the editor
   editor.getSession().setMode("ace/mode/html"); //editor mode
+  if (set_editor == 'html'){
   editor.setValue(
-    `<!DOCTYPE html>
-  <html>
-    <head>
-      <title> Document </title>
-    </head>
-    <body>
-      <p> Welcome </p>
-    </body>
-  </html>`,
+    nom_html,
     1
   ); //1 = moves cursor to end
-
+  }
+  else if (set_editor == 'Boot' || set_editor == 'boot') {
+    editor.setValue(
+      boot_html,
+      1
+    );
+  }
   editor.getSession().on("change", function () {
     update(); //updatind the output when ever anything changes
   });
@@ -36,13 +57,13 @@ function setupEditor() {
     fontSize: "16pt",
     showLineNumbers: false,
     showGutter: false,
-    vScrollBarAlwaysVisible: true,
-    enableBasicAutocompletion: false,
-    enableLiveAutocompletion: false,
+    vScrollBarAlwaysVisible: false,
+    enableBasicAutocompletion: true,
+    enableLiveAutocompletion: true,
   });
 
   editor.setShowPrintMargin(false);
-  editor.setBehavioursEnabled(false);
+  editor.setBehavioursEnabled(true);
 }
 
 // getting started
